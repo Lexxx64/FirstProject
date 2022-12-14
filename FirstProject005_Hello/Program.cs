9a42b25e2d008c67980aa10dec919b39f3d8ac67
Lexxx <simankovich@inbox.ru>
2022-12-14 22:46:16 +0400

    create FirstProject005_Hello

---
+++ FirstProject005_Hello/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ведите имя пльзователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "инна")
+{
+    Console.WriteLine("О! Это же Инна! Привет!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
